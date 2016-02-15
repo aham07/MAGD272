9fc363d82821087cd654fc53fb36a59e92f72033
aham07 <hammesaa07@uww.edu>
2016-02-14 23:37:20 -0600

    Assignment 3

---
+++ Assignment 3/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+
+    public string firstSceneName;
+    public string secondSceneName;
+    public string thirdSceneName;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void loadFirstScene(){
+        SceneManager.LoadScene(firstSceneName, LoadSceneMode.Single);
+
+    }
+
+    public void loadSecondScene()
+    {
+        SceneManager.LoadScene(secondSceneName, LoadSceneMode.Single);
+
+    }
+
+    public void loadThirdScene()
+    {
+        SceneManager.LoadScene(thirdSceneName, LoadSceneMode.Single);
+
+    }
+}
